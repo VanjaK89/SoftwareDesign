62c84c4e245f832e896a6644c0a2dc1c603d3ea5
VanjaK89 <kavgic.vanja@gmail.com>
2022-10-27 15:22:38 +0200

    push

--- v1/Uebung/Gewinnverhalten.java
+++ v1/Uebung/Gewinnverhalten.java
@@ -5,8 +5,6 @@ public class Gewinnverhalten implements Provisionverhalten{
     @Override
     public double provision(Mitarbeiter m) {
         System.out.println("Gewinverhalten::You get provision in the amount of 10% from the win");
-        double count = 0;
-        count = m.getGewinn() * 0.1;
-        return count;
+        return m.getGewinn() * 0.1;
     }
 }
--- v1/Uebung/Umsatzverhalten.java
+++ v1/Uebung/Umsatzverhalten.java
@@ -5,10 +5,7 @@ public class Umsatzverhalten implements Provisionverhalten{
     @Override
     public double provision(Mitarbeiter m) {
         System.out.println("Umsatzverhalten::You all get 5% of the salary");
-        double count = 0;
-        count = m.getUmsatz() * 0.05;
-        return count;
-
+        return  m.getUmsatz() * 0.05;
     }
 
 }
